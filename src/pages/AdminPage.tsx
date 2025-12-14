30c208d9550f0bb210120ae273ddbbd0d9ea620d
AngelicaGFlores <flores.guada.angelica@gmail.com>
2025-12-13 18:23:05 -0800

    my buttons for admin page

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AdminPage() {
   return (
     <div style={styles.page}>
-      <h1>Admin Dashboard</h1>
+      <h1 >Admin Dashboard</h1>
       <p>Per Scholas Mentorship Platform Overview</p>
 
       {/* Overview */}
@@ -52,15 +52,40 @@ export default function AdminPage() {
                 alert('Alex has been approved');
             }}  style={styles.primaryBtn}>Approve New Tutors</button>
         <button style={styles.secondaryBtn}>View Feedback </button>
-        <button style={styles.secondaryBtn}> Export Reports</button>
+        <button style={styles.thirdBTn}> Export Reports</button>
       </section>
     </div>
   );
 }
 
 const styles = {
-  page: { padding: "20px", fontFamily: "Arial" },
-  card: { background: "gray", padding: "15px", marginBottom: "20px", borderRadius: "8px" },
-  primaryBtn: { padding: "10px", marginRight: "10px", background: "#4CAF50", color: "white", border: "none", borderRadius: "5px" },
-  secondaryBtn: { padding: "10px", background: "gray", border: "none", borderRadius: "5px", background: "blue" }
+	page: { padding: "20px", fontFamily: "Arial" },
+	card: {
+		background: "gray",
+		padding: "15px",
+		marginBottom: "20px",
+		borderRadius: "8px",
+	},
+	primaryBtn: {
+		padding: "10px",
+		marginRight: "10px",
+		background: "#4CAF50",
+		color: "white",
+		borderRadius: "5px",
+	},
+	secondaryBtn: {
+		padding: "10px",
+		background: "gray",
+		border: "none",
+		borderRadius: "5px",
+    background: "red",
+    margin: "5px"
+	},
+	thirdBTn: {
+		padding: "10px",
+		background: "gray",
+		border: "none",
+		borderRadius: "5px",
+		background: "yellow",
+	},
 };
